09d7339547b61865455bde1170af5d0a653cdb88
Jeremiah Whitehurst <jalexander-wv@protonmail.com>
2025-02-12 07:54:34 -0500

    Completed both classes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
+
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
@@ -11,5 +13,43 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+        int numarrays = 3;
+        int size = 5;
+
+        int[][] result1 = ThreeWayDisjointImproved.generateSequentialIntArrays(numarrays, size);
+
+        // Displaying the generated arrays
+        for (int i = 0; i < result1.length; i++) {
+            System.out.print("Array " + (i + 1) + ": ");
+            for (int j = 0; j < result1[i].length; j++) {
+                System.out.print(result1[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        long startTime1 = System.nanoTime();
+        ThreeWayDisjointImproved.disjoint1(result1[0], result1[1], result1[2]);  // For inputArraySize1
+        long duration1 = System.nanoTime() - startTime1;
+        System.out.println("Runtime for inputArraySize2: " + duration1 + " ns");
+
+        //IMPROVED BELOW
+
+        int[][] result = ThreeWayDisjoint.generateSequentialIntArrays(numarrays, size);
+
+        // Displaying the generated arrays
+        for (int i = 0; i < result.length; i++) {
+            System.out.print("Array " + (i + 1) + ": ");
+            for (int j = 0; j < result[i].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        long startTime = System.nanoTime();
+        ThreeWayDisjoint.disjoint1(result[0], result[1], result[2]);  // For inputArraySize1
+        long duration = System.nanoTime() - startTime;
+        System.out.println("Runtime for inputArraySize1: " + duration + " ns");
     }
+
 }
---
+++ src/ThreeWayDisjoint.java
@@ -0,0 +1,53 @@
+
+public class ThreeWayDisjoint {
+
+    /**
+     * Returns true if there is no element common to all three arrays.
+     */
+    public static boolean disjoint1(int[] groupA, int[] groupB, int[] groupC) {
+        for (int a : groupA) {
+            for (int b : groupB) {
+                for (int c : groupC) {
+                    if ((a == b) && (b == c)) {
+                        return false;              // we found a common value
+
+                    }
+                }
+            }
+        }
+        return true;                               // if we reach this, sets are disjoint
+    }
+
+    /**
+     * Returns true if there is no element common to all three arrays.
+     */
+    public static boolean disjoint2(int[] groupA, int[] groupB, int[] groupC) {
+        for (int a : groupA) {
+            for (int b : groupB) {
+                if (a == b) // only check C when we find match from A and B
+                {
+                    for (int c : groupC) {
+                        if (a == c) // and thus b == c as well
+                        {
+                            return false;   // we found a common value
+
+                                }
+                    }
+                }
+            }
+        }return true;                        // if we reach this, sets are disjoint
+    }
+
+    public static int[][] generateSequentialIntArrays(int numarrays, int size) {
+        int[][] arrays = new int[numarrays][size];
+        
+        int value = 0;
+        for (int i = 0; i < numarrays; i++) {
+            for (int j = 0; j < size; j++) {
+                arrays[i][j] = value++;
+            }
+        }
+
+        return arrays;
+    }
+}
